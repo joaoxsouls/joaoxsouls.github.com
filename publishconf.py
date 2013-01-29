556333ad961821f7dfe1846658f6b09ca75fb21d
joaoxsouls <joaoxsouls@gmail.com>
2013-01-29 01:39:15 +0000

    update settings delete output files

--- publishconf.py
+++ publishconf.py
@@ -12,14 +12,5 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 # Uncomment following line for absolute URLs in production:
-RELATIVE_URLS = False
-ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
-AUTHOR_URL= 'about/'
-AUTHOR_SAVE_AS = '/about/index.html'
-CATEGORY_URL = 'category/{slug}'
-CATEGORY_SAVE_AS = 'category/{slug}/index.html'
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
