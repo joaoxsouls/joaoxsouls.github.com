82f76cc072a7fd144c537f61371266dd4805b2cc
joaoxsouls <joaoxsouls@gmail.com>
2013-01-29 00:55:17 +0000

    add source files

---
+++ content/yet-another-post.rst
@@ -0,0 +1,11 @@
+My super title
+##############
+
+:date: 2010-10-03 10:20
+:tags: thats, awesome
+:category: yeah
+:slug: my-first-post
+:author: jxs
+:summary: Short version for index and feeds
+
+bla blabla
---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'joaoxsouls'
+SITENAME = u'joaoxsouls homepage'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = False
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+AUTHOR_URL= 'about/'
+AUTHOR_SAVE_AS = '/about.html'
